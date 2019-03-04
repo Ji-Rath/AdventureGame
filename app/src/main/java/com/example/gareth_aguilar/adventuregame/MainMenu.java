601c8d95dd721d5406cfee1e6c1054ab59fbe233
gareth-aguilar <garethaguilar@gmail.com>
2019-03-04 10:05:20 -0500

    - Add main menu - Add buttons

---
+++ app/src/main/java/com/example/gareth_aguilar/adventuregame/MainMenu.java
@@ -0,0 +1,30 @@
+package com.example.gareth_aguilar.adventuregame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        Button btn_start = findViewById(R.id.btn_start);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
