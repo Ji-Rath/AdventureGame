e877d3603d077b2617cb85f4091cc5f43b904486
gareth-aguilar <garethaguilar@gmail.com>
2019-04-02 10:10:31 -0400

    - Name Changes

--- app/src/main/java/com/example/gareth_aguilar/adventuregame/MainActivity.java
+++ app/src/main/java/com/example/gareth_aguilar/adventuregame/MainActivity.java
@@ -4,14 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mainmenu);
 
         Button btn_main1 = findViewById(R.id.btn_main1);
         Button btn_main2 = findViewById(R.id.btn_main2);
--- app/src/main/java/com/example/gareth_aguilar/adventuregame/MainMenu.java
+++ app/src/main/java/com/example/gareth_aguilar/adventuregame/MainMenu.java
@@ -11,7 +11,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main);
 
         Button btn_start = findViewById(R.id.btn_start);
         Button btn_load = findViewById(R.id.btn_load);
